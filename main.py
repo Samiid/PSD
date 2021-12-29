fecf18bd4b71550dced3a675528bc9ce3f56ccc7
Samiid <93940120+Samiid@users.noreply.github.com>
2021-12-29 16:23:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ from sklearn.neighbors import KNeighborsClassifier
 
 df = pd.read_csv('Train.csv')
 st.write(df)
+
+fig, ax = plt.subplots()
+  df.hist(
+    bins=8,
+    column="Age",
+    grid=False,
+    figsize=(8, 8),
+    color="#86bf91",
+    zorder=2,
+    rwidth=0.9,
+    ax=ax,
+  )
+  st.write(fig)
