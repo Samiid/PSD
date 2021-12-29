e4126b9211573adcb10fbf60192825ad6e9b02e8
Samiid <93940120+Samiid@users.noreply.github.com>
2021-12-29 16:16:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.neighbors import KNeighborsClassifier
 
 df = pd.read_csv('Train.csv')
